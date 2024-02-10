44f9ba316282d6501969428f010ccc5991e7e1f3
Jbunz <jacob652@aol.com>
2024-02-10 08:49:57 -0800

    cleaned up divs using js to build the board cells

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const board = document.querySelector('.board');
+    const numCells = 42; 
+  
+    for (let i = 1; i <= numCells; i++) {
+      const cell = document.createElement('div');
+      cell.textContent = i;
+      board.appendChild(cell);
+    }
+  });
+  
